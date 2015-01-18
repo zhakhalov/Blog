202c827449053c1f76b56c4f758b229f631b37e5
zhakhalov <zhakhalov@gmail.com>
2015-01-18 23:06:32 +0200

    Entities added

--- Blog.Dal/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Blog.Dal.MvcApplication" Language="C#" %>
--- Blog.Entities/Properties/AssemblyInfo.cs
+++ Blog.Entities/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e4cc7130-c83f-4131-8620-eda914f38725")]
+[assembly: Guid("6fc15806-11c3-4088-bb52-885280e5953a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
